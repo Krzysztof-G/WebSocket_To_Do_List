8686066b7a755e702e78519f8da87967be86b86d
Krzysztof Gumuła <k.gumula@icloud.com>
2020-08-11 17:33:25 +0200

    add remove & adding

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express');
-// const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
-
-// Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '/client/')));
-
-// Show index.html as main page
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/index.html'));
-// });
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 })
 
 const server = app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port: 8000');
-  }); 
+console.log('Server is running on port: 8000');
+});
+  
+  const io = socket(server);
+  
+  const tasks = [];
+  
+  io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+  
+    socket.emit('updateData', tasks);
+  
+    socket.on('addTask', (taskName) => {
+      tasks.push(taskName);
+      socket.broadcast.emit('addTask', taskName);
+    });
+  
+    socket.on('removeTask', (taskId) => {
+        const index = tasks.findIndex(item => item.id === taskId);        
+        tasks.splice(index, 1);
+        socket.broadcast.emit('removeTask', taskId);
+    });
+  });
